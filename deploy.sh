711540b830147fab18ecc5cd2b2b377ebf5fe14f
g9kt <101309568+g9kt@users.noreply.github.com>
2022-03-10 11:18:35 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ REGION="hkg"
 
 if ! command -v flyctl >/dev/null 2>&1; then
     printf '\e[33mCould not resolve command - flyctl. So, install flyctl first.\n\e[0m'
-    curl -L https://fly.io/install.sh | sh | FLYCTL_INSTALL=/usr/local sh
+    curl -L https://fly.io/install.sh | FLYCTL_INSTALL=/usr/local sh
 fi
 
 if [ -z "${APP_NAME}" ]; then
@@ -31,37 +31,29 @@ fi
 printf '\e[33mNext, create app config file - fly.toml.\n\e[0m'
 cat <<EOF >./fly.toml
 app = "$APP_NAME"
-
 kill_signal = "SIGINT"
 kill_timeout = 5
 processes = []
-
 [env]
-
 [experimental]
   allowed_public_ports = []
   auto_rollback = true
-
 [[services]]
   http_checks = []
   internal_port = 443
   # processes = ["app"]
   protocol = "tcp"
   script_checks = []
-
   [services.concurrency]
     hard_limit = 50
     soft_limit = 35
     type = "connections"
-
   [[services.ports]]
     handlers = ["http"]
     port = 80
-
   [[services.ports]]
     handlers = ["tls", "http"]
     port = 443
-
   [[services.tcp_checks]]
     grace_period = "120s"
     interval = "15s"
